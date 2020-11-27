3d0f3797f3f8a04703ba871f63fd29ff5b698ba1
Веретейко Игорь Витальевич <igas89@gmail.com>
2020-11-28 00:08:31 +0300

    @доработал конфиг заголовка

---
+++ src/backend/config/httpHeaders.ts
@@ -0,0 +1,19 @@
+
+import { Request, Response, NextFunction } from 'express';
+
+interface expressRequest extends Request {
+    headers: {
+        host: string;
+        origin?: string;
+    }
+}
+
+export default {
+    'Access-Control-Allow-Origin': (req: expressRequest, res: Response, next: NextFunction) => req.headers.origin || req.headers.host,
+    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, Set-Cookie, X-Requested-With, Content-Type, Accept, X-CSRF-TOKEN',
+    'Access-Control-Allow-Credentials': true,
+    'Content-Security-Policy': (req: expressRequest, res: Response, next: NextFunction) => `connect-src 'self' ${req.headers.origin || req.headers.host}`,
+    // 'Content-Security-Policy': 'default-src *; connect-src *',
+    // 'X-Content-Security-Policy': 'default-src *; connect-src *',
+};
