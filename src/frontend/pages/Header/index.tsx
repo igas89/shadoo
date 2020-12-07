f2b33de7b8102526a30103fddd52ccaea78611dd
Веретейко Игорь Витальевич <igas89@gmail.com>
2020-12-07 20:34:40 +0300

    @url дома в лого

--- src/frontend/pages/Header/index.tsx
+++ src/frontend/pages/Header/index.tsx
@@ -1,11 +1,14 @@
 import React, { FC, memo } from 'react';
+import { Link } from 'react-router-dom';
 import './Header.scss';
 
 const Header: FC = memo(() => {
     return (
         <div className='header'>
             <div className='header__wrap'>
-                <img className='header__logo' src='/icons/logo.svg' />
+                <Link to='/'>
+                    <img className='header__logo' src='/icons/logo.svg' />
+                </Link>
                 <div className='header__title'>
                     <span>Shadoo</span>
                     <span>React TypeScript</span>
