3985257e6883885e4c72b0a56490b5a0d8743ea6
Веретейко Игорь Витальевич <igas89@gmail.com>
2020-12-20 02:39:33 +0300

    @поправлены типы

--- src/backend/handlers/v1/updateNews/index.ts
+++ src/backend/handlers/v1/updateNews/index.ts
@@ -1,5 +1,7 @@
 import BaseHandler from '../../BaseHandler';
 import storage from '../../../storage';
+import { StorageResponse } from 'types/storage';
+
 
 import { Cfg } from '../../../helpers/cfg';
 const { URL_PARSE } = Cfg('application');
@@ -13,20 +15,17 @@ export default class UpdateNewsHandler extends BaseHandler {
             maxPage: 10,
             requestLimit: 10,
         }).then((requests) => {
-            Promise
-                .all<Promise<unknown>[]>(requests)
-                .then((response) => {
-                    const data = response.reduce<unknown[]>((acc, item) => {
-                        return acc.concat(item);
-                    }, []);
+            Promise.all<StorageResponse[]>(requests).then((response) => {
+                const data = response.reduce<StorageResponse[]>((acc, item) => {
+                    return acc.concat(item);
+                }, []);
 
-                    storage.writeToCache(data).then(storageResponse => {
-                        this.sendJson({
-                            data: storageResponse,
-                        });
+                storage.writeToCache(data).then((storageResponse) => {
+                    this.sendJson({
+                        data: storageResponse,
                     });
-                })
+                });
+            });
         });
     }
 }
-
