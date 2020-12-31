32a07973ffb6ddecb61134a3457009d920af82a0
Веретейко Игорь Витальевич <igas89@gmail.com>
2020-12-31 03:59:55 +0300

    @добавлена БД sqlite3

--- src/backend/config/application.ts
+++ src/backend/config/application.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+
 /**
  * Порт бэк-сервера
  * @type {number}
@@ -11,14 +13,35 @@ export const SERVER_PORT = 3000;
 export const CACHE_FILE = 'posts.json';
 
 /**
+ * Файл базы данных
+ * @type {string}
+ */
+export const DB_FILE = 'database.db';
+
+/**
+ * Папка хранения базы данных
+ * @type {string}
+ */
+export const DB_DIR = path.join(process.cwd(), '/cache');
+
+/**
  * Папка бандлов
  * @type {string}
  */
 export const OUTPUT_DIR = 'dist';
 
-export const URL_PARSE = 'https://shazoo.ru/news';
-
 export const TOKEN = {
     secret: 'SECRET_TOKEN',
     expires: '1h',
+};
+
+export const URL_PARSE = 'https://shazoo.ru/news';
+
+/** Конфигурация парсера
+ * 
+ */
+export const PARSER_CONFIG = {
+    url: URL_PARSE,
+    maxPage: 10,
+    requestLimit: 20,
 };
---
+++ src/backend/database/index.ts
@@ -0,0 +1,102 @@
+/* eslint-disable no-console */
+import { verbose as sqliteDebug, Database, Statement, RunResult } from 'sqlite3';
+import path from 'path';
+
+import { DB_FILE, DB_DIR } from '../config/application';
+import { color } from '../helpers/common';
+
+const sqlite3 = sqliteDebug();
+
+export type DbQueryCallback = (this: Statement, err: Error | null, rows: unknown[]) => void;
+
+class Db {
+    private db: Database;
+
+    constructor() {
+        const pathDbFile = path.join(DB_DIR, `/${DB_FILE}`);
+
+        this.db = new sqlite3.Database(pathDbFile, (err): void => {
+            if (err) {
+                console.error(err.message);
+                return;
+            }
+
+            console.log(`\n${color.green}# ${color.white}Connected to the ${color.yellow}${pathDbFile}${color.white} SQlite database.`);
+        });
+    }
+
+    public initial(callback: (db?: Database) => void): void {
+        callback.call(this, this.db);
+    }
+
+    get instance(): Database {
+        return this.db;
+    }
+
+    public close(): Promise<string> {
+        return new Promise((resolve, reject) => {
+            this.db.close((err) => {
+                if (err) {
+                    reject(err.message);
+                    return;
+                }
+
+                resolve('Close the database connection.');
+            });
+        });
+    }
+
+    public all<T>(sql: string, ...params: unknown[]): Promise<T> {
+        return new Promise((resolve, reject) => {
+            this.db.all(sql, ...params, (err: Error, rows: T): void => {
+                if (err) {
+                    reject(err.message);
+                    return;
+                }
+
+                resolve(rows);
+            });
+        });
+    }
+
+    public get<T>(sql: string, ...params: unknown[]): Promise<T> {
+        return new Promise((resolve, reject) => {
+            this.db.get(sql, ...params, (err: Error, rows: T): void => {
+                if (err) {
+                    reject(err.message);
+                    return;
+                }
+
+                resolve(rows);
+            });
+        });
+    }
+
+    public prepare(sql: string, ...params: unknown[]): Promise<Statement> {
+        return new Promise((resolve, reject) => {
+            this.db.prepare(sql, ...params, function (this: Statement, err: Error | null) {
+                if (err) {
+                    reject(err.message);
+                    return;
+                }
+
+                resolve(this);
+            });
+        });
+    }
+
+    public run(sql: string, ...params: unknown[]): Promise<RunResult> {
+        return new Promise((resolve, reject) => {
+            this.db.run(sql, ...params, function (this: RunResult, err: Error | null) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                resolve(this);
+            });
+        });
+    }
+}
+
+export default new Db();
