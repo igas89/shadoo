d7545b5c85645866e9fd72eba650140aab1d3f16
Веретейко Игорь Витальевич <igas89@gmail.com>
2021-01-02 04:21:48 +0300

    @доработан хелпер даты

--- src/utils/Dates.ts
+++ src/utils/Dates.ts
@@ -1,7 +1,9 @@
+import { pluralize } from './String';
+
 export const dateISOtoString = (date: string): string => date.replace(/(\d{4})-(\d{2})-(\d{2}).*/, '$3.$2.$1');
 export const dateISOtoTime = (date: string): number => new Date(date).getTime();
 
-export const humanizeDateISO = (dateISO: string | number): string => {
+export const humanizeDate = (entryDate: string | number, convertTime = true): string => {
     const MONTHS_LIST = [
         'января',
         'февраля',
@@ -16,11 +18,28 @@ export const humanizeDateISO = (dateISO: string | number): string => {
         'ноября',
         'декабря',
     ];
-    const date = new Date(dateISO);
 
-    const formatDate = (date: string | number): string => `0${date}`.slice(-2);
-    const hours = formatDate(date.getHours());
-    const minutes = formatDate(date.getMinutes());
+    const date = new Date(entryDate);
+    let newDate = '';
 
-    return `${date.getDate()} ${MONTHS_LIST[date.getMonth()]} ${date.getFullYear()} ${hours}:${minutes}`;
+    let hours: string | number = '';
+    let minutes: string | number = '';
+
+    if (convertTime) {
+        const currentDate = new Date();
+        minutes = Math.floor((currentDate.getTime() - date.getTime()) / (1000 * 60));
+        newDate = minutes < 1
+            ? 'менее минуты назад'
+            : `${minutes} ${pluralize(minutes, 'минут', ['', 'ы', ''])} назад`;
+    }
+
+    if (!convertTime || (convertTime && minutes > 60)) {
+        const formatDate = (date: string | number): string => `0${date}`.slice(-2);
+        hours = formatDate(date.getHours());
+        minutes = formatDate(date.getMinutes());
+
+        newDate = `${date.getDate()} ${MONTHS_LIST[date.getMonth()]} ${date.getFullYear()} ${hours}:${minutes}`;
+    }
+
+    return newDate;
 };
